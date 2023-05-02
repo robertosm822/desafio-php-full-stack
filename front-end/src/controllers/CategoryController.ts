6717f66310b84a0b32a7c2aa1131516cc3829206
Roberto Soares <roberto.melo@businessintegrator.com.br>
2023-05-01 22:18:44 -0300

    Ajustes em Vue

--- front-end/src/controllers/CategoryController.ts
+++ front-end/src/controllers/CategoryController.ts
@@ -18,11 +18,32 @@ export default {
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
-    },async listAll(){
+    },
+    async listAll(){
         const res = await fetch("http://localhost:8000/api/categories/")
             .then(response => response.json())
             .then(result => result.data);
             console.log(res);
         return res;
+    },
+    async updateCategory(Id: number, NameCategory: string){
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "name": NameCategory
+        });
+
+        const requestOptions: any = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost:8000/api/categories/"+Id, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
     }
 }
